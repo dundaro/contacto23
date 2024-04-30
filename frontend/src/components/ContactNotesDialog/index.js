15a291d9f03cc8c971a490e85fea4109f1030127
dundaro <149143052+dundaro@users.noreply.github.com>
2024-04-30 09:11:20 -0500

    Update index.js

--- frontend/src/components/ContactNotesDialog/index.js
+++ frontend/src/components/ContactNotesDialog/index.js
@@ -92,7 +92,7 @@ export default function ContactNotesDialog ({ modalOpen, onClose, ticket }) {
             })
             await loadNotes()
             setNewNote({ note: '' })
-            toast.success('Observação adicionada com sucesso!')
+            toast.success('Nota agregada exitosamente!')
         } catch (e) {
             toast.error(e)
         }
@@ -110,7 +110,7 @@ export default function ContactNotesDialog ({ modalOpen, onClose, ticket }) {
             await deleteNote(selectedNote.id)
             await loadNotes()
             setSelectedNote({})
-            toast.success('Observação excluída com sucesso!')
+            toast.success('Nota eliminada exitosamente!')
         } catch (e) {
             toast.error(e)
         }
@@ -146,7 +146,7 @@ export default function ContactNotesDialog ({ modalOpen, onClose, ticket }) {
                 onClose={setShowOnDeleteDialog}
                 onConfirm={handleDelete}
             >
-                Deseja realmente excluir este registro?
+                Desea realmente eliminar este registro?
             </ConfirmationModal>
             <Dialog
                 open={open}
